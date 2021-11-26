0519ff6723b511707413933cf4e04006897365d9
Ely96 <elyjstaropoli96@gmail.com>
2021-11-25 23:51:02 -0300

    homework1

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -4,19 +4,19 @@
 const nuevaString = 'Hola';
 
 // Crea una variable numérica, puede ser cualquier número:
-const nuevoNum = null;
+const nuevoNum = 9;
 
 // Crea una variable booleana:
-const nuevoBool = null;
+const nuevoBool = true;
 
 // Resuelve el siguiente problema matemático:
-const nuevaResta = 10 - null === 5;
+const nuevaResta = 10 - 5 === 5;
 
 // Resuelve el siguiente problema matemático:
-const nuevaMultiplicacion = 10 * null === 40 ;
+const nuevaMultiplicacion = 10 * 4 === 40 ;
 
 // Resuelve el siguiente problema matemático:
-const nuevoModulo = 21 % 5 === null;
+const nuevoModulo = 21 % 5 === 1.05;
 
 
 // En los próximos 22 problemas, deberás completar la función.
@@ -25,86 +25,117 @@ const nuevoModulo = 21 % 5 === null;
 // Pista: "console.log()" NO fucionará.
 // No cambies los nombres de las funciones.
 
-function devolverString(str) {
+function devolverString(str) { return str;
   // "Return" la string provista: str
-  // Tu código:
+  // Tu código: 
   
 }
 
-function suma(x, y) {
+function suma(x, y) { return x + y;
   // "x" e "y" son números
   // Suma "x" e "y" juntos y devuelve el valor
-  // Tu código:
+  // Tu código: 
   
 }
 
-function resta(x, y) {
+function resta(x, y) { return x - y;
   // Resta "y" de "x" y devuelve el valor
   // Tu código:
   
 }
 
-function multiplica(x, y) {
+function multiplica(x, y) { return x * y;
   // Multiplica "x" por "y" y devuelve el valor
   // Tu código:
   
 }
 
-function divide(x, y) {
+function divide(x, y) { return x / y;
   // Divide "x" entre "y" y devuelve el valor
   // Tu código:
   
 }
 
-function sonIguales(x, y) {
+function sonIguales(x, y) { 
+  if (x == y) {
+    return true;
+  }
+  return false;
+} 
+  
   // Devuelve "true" si "x" e "y" son iguales
   // De lo contrario, devuelve "false"
   // Tu código:
   
-}
 
-function tienenMismaLongitud(str1, str2) {
+
+function tienenMismaLongitud(str1, str2) { 
+  if (str1.lenght == str2.lenght) {
+    return true;
+  }
+  return false;
+}
   // Devuelve "true" si las dos strings tienen la misma longitud
   // De lo contrario, devuelve "false"
   // Tu código:
   
-}
 
-function menosQueNoventa(num) {
+
+function menosQueNoventa(num) { 
+  if (num < 90 ) {
+    return true;
+  }
+  return false;
+}
   // Devuelve "true" si el argumento de la función "num" es menor que noventa
   // De lo contrario, devuelve "false"
   // Tu código:
   
-}
+
 
 function mayorQueCincuenta(num) {
+  if (num > 50) {
+    return true;
+  }
+  return false;
+  } 
   // Devuelve "true" si el argumento de la función "num" es mayor que cincuenta
   // De lo contrario, devuelve "false"
   // Tu código:
   
-}
 
-function obtenerResto(x, y) {
+
+function obtenerResto(x, y) { return x / y == numero;
   // Obten el resto de la división de "x" entre "y"
   // Tu código:
   
 }
 
-function esPar(num) {
+function esPar(num) { 
+  if (num % 2 === 0) {
+    return true;
+  } 
+  return false;
+}
   // Devuelve "true" si "num" es par
   // De lo contrario, devuelve "false"
   // Tu código:
   
-}
+
 
 function esImpar(num) {
+  if (num 2 % === 1) {
+    return true;
+} 
+  return false;
+}
   // Devuelve "true" si "num" es impar
   // De lo contrario, devuelve "false"
   // Tu código:
   
-}
 
-function elevarAlCuadrado(num) {
+
+function elevarAlCuadrado(num) { return num * 2;  
   // Devuelve el valor de "num" elevado al cuadrado
   // ojo: No es raiz cuadrada!
   // Tu código:
